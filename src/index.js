0212bd4c905e27cded972712ca1a7aaf7b86e7ef
Vetrichelvan Jeyapalpandy <vetri02@gmail.com>
2019-04-07 23:22:03 +0530

    Init

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Shoes",
-    description: "A pair of warm, fuzzy socks"
+    description: "A pair of warm, fuzzy socks",
+    href: "www.google.com",
+    image: "/src/vmSocks-green-onWhite.jpg",
+    inStock: true,
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue"
+      }
+    ]
   }
 });
