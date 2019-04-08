51cd191d605c67fd1bbcbf448154b99c963b6785
Vetrichelvan Jeyapalpandy <vetri02@gmail.com>
2019-04-09 01:22:38 +0530

    Components

--- src/index.js
+++ src/index.js
@@ -1,30 +1,85 @@
-var app = new Vue({
-  el: "#app",
-  data: {
-    brand: "Vue Mastery",
-    product: "Shoes",
-    description: "A pair of warm, fuzzy socks",
-    href: "www.google.com",
-    selectedVariant: 0,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    variants: [
-      {
-        variantId: 2234,
-        variantColor: "green",
-        variantImage:
-          "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
-        variantQuantity: 100,
-        onSale: true
-      },
-      {
-        variantId: 2235,
-        variantColor: "blue",
-        variantImage:
-          "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg",
-        variantQuantity: 0
-      }
-    ],
-    cart: 0
+Vue.component("product-details", {
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `<ul>
+  <li v-for="detail in details">{{detail}}</li>
+</ul>`
+});
+
+Vue.component("product", {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: `<div class="product">
+  <div class="product-image">
+    <img v-bind:src="image" />
+  </div>
+  <div class="product-info">
+    <div class="cart">
+      Cart {{cart}}
+    </div>
+    <h1>
+      {{title}}
+      <span v-if="onSale" class="onSale">On Sale</span>
+    </h1>
+    <p>{{description}}</p>
+    <p v-if="inStock">In Stock</p>
+    <p v-else :style="{textDecoration: 'line-through'}">Out of Stock</p>
+    <p>Shipping: {{shipping}}</p>
+    <product-details :details="details"></product-details>
+
+    <div
+      class="color-box"
+      v-for="(variant, index) in variants"
+      :key="variant.variantId"
+      :style="{backgroundColor: variant.variantColor}"
+      @mouseover="updateProduct(index)"
+    ></div>
+
+    <button
+      v-on:click="addToCart"
+      :disabled="!inStock"
+      :class="{disabledButton: !inStock}"
+    >
+      Add to cart
+    </button>
+    <button v-on:click="removeFromCart">Remove from cart</button>
+  </div>
+</div>`,
+  data() {
+    return {
+      brand: "Vue Mastery",
+      product: "Shoes",
+      description: "A pair of warm, fuzzy socks",
+      href: "www.google.com",
+      selectedVariant: 0,
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: "green",
+          variantImage:
+            "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+          variantQuantity: 100,
+          onSale: true
+        },
+        {
+          variantId: 2235,
+          variantColor: "blue",
+          variantImage:
+            "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg",
+          variantQuantity: 0
+        }
+      ],
+      cart: 0
+    };
   },
   methods: {
     addToCart() {
@@ -52,6 +107,20 @@ var app = new Vue({
     },
     onSale() {
       return this.variants[this.selectedVariant].onSale;
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free";
+      } else {
+        return "$2.99";
+      }
     }
+  }
+});
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    premium: false
   }
 });
